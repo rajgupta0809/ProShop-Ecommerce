b1d5fbd05ecbb3b71bba6b33063bf29dad3fd525
Raj Gupta <rgm5823@gmail.com>
2022-07-08 14:32:21 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 const App = () => {
   return (
-    <>
-      <h1>Proshop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container className='py-3'>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
